866fe5c7652e4467f952208ccc014f2a64c21b0e
Erich von Hauske <ehauske@gmail.com>
2017-05-13 10:29:24 -0500

    Added _FILE evn variables support for docker secrets. Changed install to download from my github

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,18 @@ FROM debian:wheezy
 
 MAINTAINER George Haidar <ghaidar0@gmail.com>
 
-ENV MOP_DB_URL mongodb://localhost/local
-ENV MOP_BUS_URL amqp://localhost
-ENV MOP_BUS_CHANNEL objects
-
 RUN set -e; \
     apt-get update; \
     apt-get install -y build-essential curl; \
     curl -sL https://deb.nodesource.com/setup_6.x | bash -; \
+    apt-get install -y mongodb-clients; \
     apt-get install -y nodejs; \
     apt-get clean; \
     rm -rf /var/lib/apt/lists/*
 
-RUN npm install -g mongodb-oplog-publisher forever
+    RUN npm install -g git+https://git@github.com/erichmx/mongodb-oplog-publisher.git
+    RUN npm install -g forever
 
-CMD ["forever", "--minUptime", "1000", "--spinSleepTime", "1000", "/usr/bin/mop"]
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+#CMD ["forever", "--minUptime", "1000", "--spinSleepTime", "1000", "/usr/bin/mop"]
+ENTRYPOINT ["bash","/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,30 @@
+echo Starting mongodb-oplog-publisher
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	echo "Reading ${var}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+while read i; do file_env $i; done < <(env | grep _FILE= | sed -n 's/\(.*\)_FILE=.*/\1/p')
+# SECURITY HOLE PRINTING ENV AFTER LOADING SECRETS, DON'T DO IT!! printenv
+
+mongo --version
+forever --minUptime 1000 --spinSleepTime 1000 /usr/bin/mop
